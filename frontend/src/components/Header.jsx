24c266b799b9ae9bd9c53efe925d9b01a519f722
nisha-muthurajan <deltadelta5382@gmail.com>
2025-10-05 12:21:57 +0530

    feat: Add react-icons for location and search in Header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,15 +2,20 @@
 
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { MdLocationOn, MdSearch } from "react-icons/md";
 
 function Header() {
   return (
     <header className="header">
       <div className="logo"></div>
       <div className="search-area">
-        <span className="location-pin">📍</span>
+        <span className="location-pin">
+          <MdLocationOn size={20} />
+        </span>
         <input type="text" placeholder="Search location..." />
-        <button className="search-button">🔍</button>
+        <button className="search-button">
+          <MdSearch size={20} />
+        </button>
       </div>
       <div className="tabs">
         <NavLink
